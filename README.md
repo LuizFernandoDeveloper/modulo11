dab76cc550b8bb04d16b9fa8b5f9bf189b9faf81
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-08-03 14:10:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# modulo12
+# modulo11
---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            Set<String> masculino = new HashSet<>();
+            Set<String> feminino = new HashSet<>();
+
+            while (true) {
+                System.out.print("Digite o nome (ou 'sair' para encerrar): ");
+                String nome = scanner.nextLine();
+                if (nome.equalsIgnoreCase("sair")) {
+                    break;
+                }
+
+                System.out.print("Digite o sexo (M/F): ");
+                String sexo = scanner.nextLine();
+
+                if (sexo.equalsIgnoreCase("M")) {
+                    masculino.add(nome);
+                } else if (sexo.equalsIgnoreCase("F")) {
+                    feminino.add(nome);
+                } else {
+                    System.out.println("Sexo inválido. Digite 'M' para masculino ou 'F' para feminino.");
+                }
+            }
+
+            System.out.println("\n--- Grupo Masculino ---");
+            if (!masculino.isEmpty()) {
+                for (String nome : masculino) {
+                    System.out.println(nome);
+                }
+            } else {
+                System.out.println("Nenhum nome masculino registrado.");
+            }
+
+            System.out.println("\n--- Grupo Feminino ---");
+            if (!feminino.isEmpty()) {
+                for (String nome : feminino) {
+                    System.out.println(nome);
+                }
+            } else {
+                System.out.println("Nenhum nome feminino registrado.");
+            }
+        }
+    }
+}
